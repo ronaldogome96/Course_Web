5ed3a8ea471c382df399ff230766a7006590c5b6
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 14:51:47 -0300

    Aula 18

---
+++ JS/Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+//Algumas curiosidades de js
+console.log(7/0) // Printa infinity
+console.log("10" / 2) // Printa 5
+console.log("Show"*2) // Printa NaN
+console.log(0.1+0.7) //0.7999999999
+//console.log(10.toString()) ERRO
+console.log((10.3245).toFixed(2))
+console.log((10.35144).toString())
