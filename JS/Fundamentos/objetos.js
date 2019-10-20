6e1e61cde1f41492e923d4292ae5b667bbee898b
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 17:39:41 -0300

    Aula 24

---
+++ JS/Fundamentos/objetos.js
@@ -0,0 +1,13 @@
+const prod1 = {} // cria um objeto vazio
+prod1.nome= "PS4" // forma de adicionar parametros ao objeto
+prod1.preco = 45.57469
+prod1['desconto legal']= 0.40 // evitar atributos com espaços
+
+console.log(prod1)
+
+// outra forma de criar um objeto
+const prod2 = {
+    nome: "Camisa" ,
+    preco: 79.147
+}
+console.log(prod2)
