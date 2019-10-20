cf7eac0947407c6445d86fe916e8291f1a1efb99
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 20:24:43 -0300

    Aula 45

---
+++ JS/Fundamentos/relacionais.js
@@ -0,0 +1,7 @@
+console.log('01}' , '1' == 1)  // True, pois compara o valor
+console.log('02}', '1'===1) // False pois compara o valor e tipo
+
+console.log('03}' , '3'!=3) //False
+console.log('04}','3'!=3 ) //true
+
+console.log('05}', undefined==null) //true pois é o valor
