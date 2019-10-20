b2fa11251ad723fa3dd5470cb1709fb89e705b73
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 14:04:53 -0300

    Aula 14

---
+++ JS/Fundamentos/comentario.js
@@ -0,0 +1,14 @@
+//  Comentario de uma linha
+console.log('Linha 1')
+/* 
+Comentario de 
+Multiplas linhas
+*/
+console.log('Linha 2')
+/*
+*Comentario
+*de
+*Multiplas
+*Linhas
+*/
+console.log('Linha 3')
