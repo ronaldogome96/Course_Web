70b998e5ba624261051618fb7e3ecca286d95958
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 14:19:38 -0300

    Aula 15

---
+++ JS/Fundamentos/VariaveisEConstantes.js
@@ -0,0 +1,16 @@
+
+//var é uma palavra reservada do js para declarar uma variavel 
+var a = 3
+
+//let é outra palavra reservada para declarar variaveis
+let b = 4
+
+var a = 40 // var pode ser redeclarada
+// let b = 70 isso gera um erro
+b = 70
+
+console.log(a,b)
+
+const c = 5 // palavra reservada para constante
+
+console.log(c)
