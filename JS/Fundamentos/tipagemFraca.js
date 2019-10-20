6370709ff61cbb89301c62fef41cfbbfda183d3f
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 14:28:56 -0300

    Aula 16

---
+++ JS/Fundamentos/tipagemFraca.js
@@ -0,0 +1,17 @@
+let qualquer = 'Legal' // declara uma variavel string
+
+console.log(qualquer) // Imprime essa variavel
+
+console.log(typeof qualquer) //typeof Informa o tipo da variavel
+
+//JS é fracamente tipada
+qualquer = 3.1548 // Mudou o tipo da variavel
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar escolher nomes genericos
+let valor= ''
+let numero=1
+let pqp=false 
+//Nao usar nomes genericos no codigo
+
