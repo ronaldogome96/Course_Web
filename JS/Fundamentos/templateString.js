1dd66eef340f92e02a2cf14edab9e18cef612005
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 16:51:53 -0300

    Aula 21

---
+++ JS/Fundamentos/templateString.js
@@ -0,0 +1,9 @@
+
+const nome = "Ronaldo"
+const concatena="Ola " + nome + "!!!!"
+
+const template=`Ola ${nome}`  // Template é esse tipo de concatenação
+console.log(concatena, template)
+const up = texto => texto.toUpperCase()
+console.log(`eiiii ${up('cuidado')}`) //Função de imprimir a palvar com letra maiscula
+
