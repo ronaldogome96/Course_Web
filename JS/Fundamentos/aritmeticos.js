8687a5fee594ab3258d213e74f13c8de5642cc0f
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2019-10-20 20:16:36 -0300

    Aula 44

---
+++ JS/Fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [3,5,8,9]
+const soma =a+b+c+d
+
+const sub = d-b
+const mul = a*b
+const div = a/c
+const mod = a%2
+
+console.log(soma, sub, mul, -div, mod) //-div faz do valor final negativo
